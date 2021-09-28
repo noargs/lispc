90df3aee0704dfdc1e93e6eabc5ac3647b9c1965
Ibn Asghar <asim.ibn.asghar@gmail.com>
2021-09-28 21:07:13 +0100

    lispc repl i/o

--- main.c
+++ main.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 
-int main() {
-    printf("Hello, World!\n");
+static char input[2048];
+
+int main(int argc, char** argv) {
+
+    puts("Lispc version 0.0.0.1");
+    puts("Press Ctrl+c to exit\n");
+
+    while(1){
+        fputs("lispc> ", stdout);
+
+        fgets(input, 2048, stdin);
+
+        printf("%s", input);
+    }
+
     return 0;
 }
